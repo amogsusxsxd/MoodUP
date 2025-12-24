2b46b7f36dd3210ec7d2b88990c1036043d949fc
amogsusxsxd <154699002+amogsusxsxd@users.noreply.github.com>
2025-12-24 13:30:47 +0300

    Delete mood_app directory

--- mood_app/models.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-
-db = SQLAlchemy()
-
-class Record(db.Model):
-    id    = db.Column(db.Integer, primary_key=True)
-    mood  = db.Column(db.String(20), nullable=False)   # happy, sad, angry, calm
-    stamp = db.Column(db.Date, default=datetime.utcnow)
